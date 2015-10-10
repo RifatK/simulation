c632edde883f9e85a6864753f860dc083d5f684c
RifatK <markovian.r@gmail.com>
2015-10-10 17:08:30 -0400

    Approximate the Number Pi with uniform random

---
+++ approximate_pi.erl
@@ -0,0 +1,13 @@
+-module(easy).
+-export([get_pi/1,approximate_pi/1]).
+
+
+get_pi(N) when N > 1 ->
+  approximate_pi(N)/N;
+get_pi(N) when N >=0 ->
+  approximate_pi(1)/2. 
+
+approximate_pi(N) when N > 0 ->
+  (4 * math:sqrt(1 - math:pow(random:uniform(),2))) + approximate_pi(N-1);
+approximate_pi(0) ->
+  (4 * math:sqrt(1 - math:pow(random:uniform(),2))).
